bdfc3680e5335f6fd92e7aa048667bdc547f7c61
dave <michael828inoc@gmail.com>
2021-11-20 15:42:12 +0800

    ui changes

---
+++ src/modules/loginPage/LoginPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../../ui/buttons/Button";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Heading>Log in Page</Heading>
+      <Button>Click Me!</Button>
+    </div>
+  );
+};
+
+export default LoginPage;
+
+const Heading = styled.h1`
+  color: ${(props) => props.theme.colors.red};
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,3 @@
-import React from "react";
-import styled from "styled-components";
-import Button from "../ui/buttons/Button";
-
-const LoginPage = () => {
-  return (
-    <div>
-      <Heading>Log in Page</Heading>
-      <Button>Click Me!</Button>
-    </div>
-  );
-};
+import LoginPage from "../modules/loginPage/LoginPage";
 
 export default LoginPage;
-
-const Heading = styled.h1`
-  color: ${(props) => props.theme.colors.red};
-`;
