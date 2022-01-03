c920e9b9cf52888037378e2e8ca0bc335e684967
Dave Inoc <michael828inoc@gmail.com>
2022-01-04 02:09:20 +0800

    make svg work

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = {
       "external-content.duckduckgo.com",
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
