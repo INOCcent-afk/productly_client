4e7b473cb7257ac7a05101cf8ddfe1e2ad99d491
INOCcent-afk <michael828inoc@gmail.com>
2025-02-10 22:10:41 +0800

    test prettierrc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,40 @@
-import "../styles/global.css";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import store from "../redux/store";
-import { ThemeProvider } from "styled-components";
-import theme from "../utils/theme";
-import Layout from "../shared-components/Layout";
-import "react-toastify/dist/ReactToastify.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import GlobalStyles from "../styles/GlobalStyles";
+import '../styles/global.css'
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
+import { ThemeProvider } from 'styled-components'
+import theme from '../utils/theme'
+import Layout from '../shared-components/Layout'
+import 'react-toastify/dist/ReactToastify.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: false,
-      staleTime: 5 * 60 * 1000,
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            retry: false,
+            staleTime: 5 * 60 * 1000,
+        },
     },
-  },
-});
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen={true} />
+                <ThemeProvider theme={theme}>
+                    <GlobalStyles />
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ThemeProvider>
+            </QueryClientProvider>
+        </Provider>
+    )
 }
 
-export default MyApp;
+export default MyApp
